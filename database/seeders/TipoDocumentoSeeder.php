150eda5e145dbe5f705e56e03ef920b4f395912c
PabloLms <pablolomusan@hotmail.com>
2021-11-19 08:51:52 -0500

    PROYEC-2 Tipos de Documentos por defecto

---
+++ database/seeders/TipoDocumentoSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Configuracion\TipoDocumento;
+use Illuminate\Database\Seeder;
+
+class TipoDocumentoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        TipoDocumento::create([
+            'tipo'=>'Boleta de Venta'
+        ]);
+        TipoDocumento::create([
+            'tipo'=>'Factura de Venta'
+        ]);
+    }
+}
